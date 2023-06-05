f7290961f8a6ae8e0dbc23372da90efcf9ccee64
Carlos Garcia <carlosgarcia3995@gmail.com>
2023-06-05 15:39:53 -0700

    Refactor getDate function

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let items = ["Buy food", "Cook food", "Eat food"];
-let workItems = [];
+const items = ["Buy food", "Cook food", "Eat food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res) {
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let today = new Date();
 
-    let options = {
-        weekday: "long", 
-        day: "numeric",
-        month: "long"
-    };
 
-    let day = today.toLocaleDateString("en-US", options);
-
+    const day = date.getDate();
     res.render("list", {listTitle: day, newListItems: items});
 
 });
@@ -30,7 +23,7 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
 
     console.log(req.body);
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if (req.body.list === "Work") {
         workItems.push(item);
